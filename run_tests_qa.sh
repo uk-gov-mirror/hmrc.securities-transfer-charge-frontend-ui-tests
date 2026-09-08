82a00ddd16ba8861c3b8f8ec22ff032736347236
karthik-a-j <141819159+karthik-a-j@users.noreply.github.com>
2026-09-08 15:12:12 +0100

    update .sh files

--- run_tests_qa.sh
+++ run_tests_qa.sh
@@ -2,5 +2,5 @@
 
 BROWSER=$1
 ENVIRONMENT=$2
-sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=qa}" -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs.S1RegistrationIndividualSpec uk.gov.hmrc.ui.specs.S2RegistrationBusinessSpec uk.gov.hmrc.ui.specs.S3RegistrationIndividualKOSpec uk.gov.hmrc.ui.specs.S4RegistrationBusinessKOSpec" testReport
+sbt scalafmtAll scalafmtCheckAll scalafmtSbtCheck clean compile -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=qa}" -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.ui.specs.*" testReport
 
